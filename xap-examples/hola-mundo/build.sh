fccae68da4c737dba6893d0bd601ca5d4554b7a4
moran <moran@gigaspaces.com>
2016-07-13 16:30:05 +0300

    Hola Mundo example, build.bat

--- xap-examples/hola-mundo/build.sh
+++ xap-examples/hola-mundo/build.sh
@@ -3,7 +3,7 @@
 DIR_NAME=$(dirname ${BASH_SOURCE[0]})
 
 if [ "${M2_HOME}" = "" ] ; then
-    M2_HOME="${XAP_HOME}/tools/maven/apache-maven-3.2.5"; export M2_HOME
+    M2_HOME="../../tools/maven/apache-maven-3.2.5"; export M2_HOME
 fi
 
 if [ -z "$1" ] || ([ $1 != "clean" ] && [ $1 != "compile" ] && [ $1 != "package" ] && [ $1 != "run-translator" ] && [ $1 != "run-feeder" ] && [ $1 != "run-partitioned-translator" ] && [ $1 != "intellij" ]); then
@@ -18,7 +18,7 @@ if [ -z "$1" ] || ([ $1 != "clean" ] && [ $1 != "compile" ] && [ $1 != "package"
   echo "run-translator              --> Starts the translator (single data-grid)"
   echo "run-feeder                  --> Starts the phrase feeder"
   echo "run-partitioned-translator  --> Starts a partitioned translator (data-grid of 2 partitions)"
-  echo "intellij                    --> Creates run configuration for IntelliJ IDE"
+  echo "intellij                    --> Copies run configuration into existing .idea IntelliJ IDE folder"
   echo
   echo "**note:** IntelliJ command assumes that the new IntelliJ project is under the examples root"
   echo "          directory, e.g. the .idea hidden folder is at this location."
@@ -42,7 +42,7 @@ elif [ $1 = "run-feeder" ]; then
   ../../bin/pu-instance.sh -path ${DIR_NAME}/feeder/target/hola-mundo-feeder.jar
 
 elif [ $1 = "run-partitioned-translator" ]; then
-../../bin/pu-instance.sh -path ${DIR_NAME}/translator/target/hola-mundo-translator.jar -cluster schema=partitioned total_members=2,0
+  ../../bin/pu-instance.sh -path ${DIR_NAME}/translator/target/hola-mundo-translator.jar -cluster schema=partitioned total_members=2,0
 
 elif [ $1 = "intellij" ]; then
   cp -r $DIR_NAME/runConfigurations $DIR_NAME/.idea
